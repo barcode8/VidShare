b0f12e85b4b912fc4c8337697220439ce00dba94
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-08-21 13:43:03 +0530

    Add pre-delete hook to clean up likes and replies when a comment is deleted

--- Backend/src/models/comment.models.js
+++ Backend/src/models/comment.models.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose"
+import { Model } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new Schema({
@@ -24,6 +25,29 @@ const commentSchema = new Schema({
     }
 
 },{timestamps:true})
+
+commentSchema.pre('deleteOne', {document : true, query : false}, async function (next){
+    const commentId = this._id;
+
+    try {
+        //When a comment is deleted, we need to delete all associated likes with the comment too for proper cleanup. We use mongoose.Model to prevent circular dependency issues
+        await mongoose.model("Like").deleteMany({ comment: commentId});
+
+        //Find all child replies associated with the comment
+        const replies = await mongoose.model("Comment").find({ parentComment : commentId})
+
+        //Just like the Likes, we need to delete all replies associated to this comment too
+        for(const reply of replies){
+            await reply.deleteOne();
+        }
+
+        //Even though deleteMany would be faster compared to using deleteOne on each iteration, we use deleteOne so that whenever a reply comment object is deleted, this hook fires AGAIN and prunes all the replies to the reply
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
 
 commentSchema.plugin(mongooseAggregatePaginate)
 
